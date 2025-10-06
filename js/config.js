896ddb1b43463527942d21f1ae69e228885e93c9
Gaurav kumar <gauravsbs2@gmail.com>
2025-10-07 01:19:01 +0530

    initial commit

---
+++ js/config.js
@@ -0,0 +1,26 @@
+/******************************************************************************************************************/
+/* DEVIN | Responsive Personal Portfolio Theme                                                                    */
+/* (c) Rene Puschinger | https://themeforest.net/user/themegrids                                                  */
+/******************************************************************************************************************/
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = [
+    "I'm an experienced <strong>Fullstack Developer</strong> <br /> and <strong>UI/UX Designer</strong>.",
+    "I build <em>responsive</em> and <em>interactive websites</em>.",
+    "Passionate about <span style='color:#f2ca0d'>modern technologies</span>."
+];
+
+
+/* Disable Parallax effect on touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Enable the Particles effect */
+var config_enableParticles = false;
+
+/* Google map parameters */
+var config_mapLat = 23.3721; // latitude
+var config_mapLng = 85.3383; // longitude
+var config_mapZoom = 16; // map zoom
+var config_mapTitle = 'My Location'
